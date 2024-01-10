4a0d0c043ab29cedbc9933c656e4c61d54e87bfa
Rithk <rithkchaudhary86@gmail.com>
2024-01-10 10:54:16 +0530

    code1

--- Lab1/table.cpp
+++ Lab1/table.cpp
@@ -9,9 +9,10 @@ int main()
     cout<<"Enter the multiplier"<<endl;
     cin>>multiplier;
      float multiplier2=multiplier;
+     cout<<"The table is"<<endl;
     for(int i=1;i<=10;i++){
         
-      cout<<tableNumber*multiplier2<<endl;
+      cout<<tableNumber<<" * "<<multiplier2<<" = "<<tableNumber*multiplier2<<endl;
       multiplier2=multiplier2+multiplier;
 
     }
