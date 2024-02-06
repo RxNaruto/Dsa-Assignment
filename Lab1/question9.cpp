fd082a38bbd9ea63de78363f61fdf40f80c67ad3
Rithk <rithkchaudhary86@gmail.com>
2024-02-06 19:18:54 +0530

    question 9

--- Lab1/question9.cpp
+++ Lab1/question9.cpp
@@ -1,24 +1,31 @@
-// This question is incomplete
-
 #include<iostream>
-#include<set>
-#include<vector>
 using namespace std;
-int main()
-{
-   vector<int>arr={1,0,2,1,3,0,4};
+void overlapping(int arr[],int size,int i){
+    for(int j=i;j<size-1;j++){
+        arr[j]=arr[j+1];
+    }
+    arr[size-1]=0;
+}
+void removingDuplicates(int arr[],int& size){
+    for(int i=0;i<size-1;i++){
+        for(int j=i+1;j<size;){
+            if (arr[j]==arr[i]){
+                overlapping(arr,size,j);
+                size--;
+            } else{
+                j++;
+            }
+        }
+    }
+}
 
-   set<int>removeDup(arr.begin(),arr.end());
+int main(){
+    int arr[7]={1,2,0,1,3,1,2};
+    int size=7;
+    removingDuplicates(arr,size);
+    for(int i=0; i<size;i++){
+        cout<<arr[i]<<" ";
+    }
 
-   vector<int>uniqueVector(removeDup.begin(),removeDup.end());
-
-   for(int num : arr){
-    cout<<num<<" ";
-   }
-   cout<<endl;
-   for(int num: uniqueVector){
-    cout<<num<<" ";
-   }
-
- return 0;
+    return 0;
 }
