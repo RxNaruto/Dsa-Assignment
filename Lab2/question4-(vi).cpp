185cb1ea294a91a2a0cc5eb73ec5cdc08434a464
Rithk <rithkchaudhary86@gmail.com>
2024-02-21 11:14:45 +0530

    lab3

--- Lab2/question4-(vi).cpp
+++ Lab2/question4-(vi).cpp
@@ -10,6 +10,22 @@ class node{
         next=nullptr;
     }
 };
+
+void mergingLL(node* head1,node* head2){
+    node* ptr1=head1;
+    node* ptr2=head2;
+    node* dummynode = new node(-1);
+    node* ptr3=dummynode;
+    while(ptr1!=nullptr && ptr2!=nullptr){
+        ptr3->next=ptr1;
+        ptr1=ptr1->next;
+        ptr3=ptr3->next;
+        ptr3->next=ptr2;
+        ptr2=ptr2->next;
+
+
+    }
+}
 int main()
 {
     node* n1=new node(1);
@@ -22,6 +38,8 @@ int main()
     node* p2=new node(4);
     node* p3=new node(6);
     node* p4=new node(8);
+
+
 
  return 0;
 }
---
+++ Lab3/Question-1.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int lengthOfString(string s){
+    int count=0;
+    int i=0;
+    while(s[i]!=0){
+        count++;
+        i++;
+    }
+    return count;
+}
+void stringToLowerCase(string& s){
+    int i=0;
+    while(s[i]!=0){
+        s[i]=(char)tolower(s[i]);
+        i++;
+    }
+}
+int NumberOfVowels(string s){
+    stringToLowerCase(s);
+    int i=0,count=0;
+    while(s[i]!=0){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' ){
+            count++;
+            i++;
+        }
+        else{
+            i++;
+        }
+        
+    }
+    return count;
+}
+bool onlyContainAlphabet(string s){
+    int i=0;
+    while(s[i]!=0){
+        if(!(s[i]>='a' && s[i]<='z')){
+            return false;
+            break;
+        }
+        else{
+            i++;
+        }
+    }
+    return true;
+}
+void reverseString(string& s){
+    int length=lengthOfString(s);
+    if(length%2==0){
+        int end=length-1;
+        for(int i=0;i<(length/2);i++){
+            char temp=s[i];
+            s[i]=s[end];
+            s[end]=temp;
+            end--;
+
+        }
+    }
+    else{
+        int end=length-1;
+        for(int i=0;i<(length/2);i++){
+            char temp=s[i];
+            s[i]=s[end];
+            s[end]=temp;
+            end--;
+
+        }
+        
+    }
+}
+bool palindrome(string s){
+    int length=lengthOfString(s);
+    if(length%2==0){
+        int end=length-1;
+        for(int i=0;i<(length/2);i++){
+           if(s[i]!=s[end]){
+            return false;
+            
+           }
+           else{
+            end--;
+           }
+
+        }
+    }
+    else{
+        int end=length-1;
+        for(int i=0;i<(length/2);i++){
+            if(s[i]!=s[end]){
+                return false;
+            }
+            else{
+                end--;
+            }
+        }
+        
+    }
+    return true;
+
+}
+int main()
+{
+    string s;
+    cout<<"Enter the string"<<endl;
+    cin>>s;
+    //cout<<"The length of string is "<<lengthOfString(s);
+    // stringToLowerCase(s);
+    // cout<<s;
+    // int length=lengthOfString(s);
+    // int vowels=NumberOfVowels(s);
+    // int words=length-vowels;
+
+    // cout<<"The number of vowels in a string are "<<vowels<<" words are "<< words;
+    // cout<<"The string only contain alphabet "<<onlyContainAlphabet(s);
+    // reverseString(s);
+    // cout<<s;
+    cout<<"The string is palindrom"<<palindrome(s);
+
+
+    
+ return 0;
+}
